63498480126040a4b6accdd99c2253c5ad7ea8a0
Szymański Łukasz <szymanski.lukasz88@gmail.com>
2015-04-04 21:09:03 +0200

    Removed unused favicon

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var nunjucks  = require('nunjucks');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
