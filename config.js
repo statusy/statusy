143e11766d014af9aa561161aed15a66b74bc804
Szymański Łukasz <szymanski.lukasz88@gmail.com>
2015-04-06 23:58:07 +0200

    Improved config

--- config.js
+++ config.js
@@ -1,43 +1,29 @@
-var config, statuses;
+var statuses = require('./statuses');
 
-statuses = {
-  active : {
-    message: "Active"
-  },
-  deploy: {
-    message: "Deploy"
-  },
-  error: {
-    message: "Error"
-  },
-  worker: {
-    waiting: "Waiting for deploy end"
-  }
-};
+var config = {};
 
-config = {
-  DELAY_CHECK_SITES: 60 * 1000,
-  statuses: statuses,
-  sites: [
-    {
-      host: 'http://example.com',
-      name: 'example.com',
-      status: statuses.active.message,
-      last_deploy_date: "N/A"
-    },
-    {
-      host: 'http://example2.com',
-      name: 'example2.com',
-      status: statuses.active.message,
-      last_deploy_date: "N/A"
-    },
-    {
-      host: 'http://example3.com',
-      name: 'example3.com',
-      status: statuses.active.message,
-      last_deploy_date: "N/A"
-    },
-  ]
-};
+config.DELAY_CHECK_SITES = 5 * 1000; // in miliseconds
+
+config.statuses = statuses;
+config.sites = [
+  {
+    host: 'http://example.com',
+    name: 'example.com',
+    status: statuses.active.message,
+    last_deploy_date: "N/A"
+  },
+  {
+    host: 'http://example2.com',
+    name: 'example2.com',
+    status: statuses.active.message,
+    last_deploy_date: "N/A"
+  },
+  {
+    host: 'http://example3.com',
+    name: 'example3.com',
+    status: statuses.active.message,
+    last_deploy_date: "N/A"
+  },
+];
 
 module.exports = config;
---
+++ statuses.js
@@ -0,0 +1,14 @@
+module.exports = {
+  active : {
+    message: "Active"
+  },
+  deploy: {
+    message: "Deploy"
+  },
+  error: {
+    message: "Error"
+  },
+  worker: {
+    waiting: "Waiting for deploy end"
+  }
+};
